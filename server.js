2a4962b46d2997fa6a74116c62b1b1306ba3df7f
Nickolay <nickarbuzov@yandex.by>
2024-05-15 01:50:15 +0300

    feat: update

--- server.js
+++ server.js
@@ -1,26 +1,37 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
+
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
         origin: '*', 
-        methods: ["GET", "POST"],
+        methods: ["GET", "POST"]
     },
-    transports: ['websocket', 'polling'],
+    transports: ['websocket', 'polling']
 });
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', channel => {
-    channel.on('message', message => {
-        channel.broadcast.emit('message', message);
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+
+    socket.on('message', (message) => {
+        console.log('Received message:', message);
+        socket.broadcast.emit('message', message);
     });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
